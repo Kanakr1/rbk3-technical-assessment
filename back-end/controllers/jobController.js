d75f33086b166276dd88fadd2a08549a718ddd7d
tariq-alhariri <tariqahmad1987@gmail.com>
2017-09-27 10:05:59 +0300

    after adding salary query

--- back-end/controllers/jobController.js
+++ back-end/controllers/jobController.js
@@ -1,10 +1,13 @@
 
 // Create a new job in the database
+
 exports.createJob = function (job, callback) {
   // TODO
+
 };
 
 // Get all jobs that have a salary greater than $50,000
 exports.getHighPayingJobs = function (callback) {
   // TODO
+  db.collection.find( { salary: { $gt: 50000} } )
 };
