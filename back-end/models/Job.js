d2f1e122d3141580cb19cad854fcc832c4f816aa
RBK8 <maisaa.alkhedr@gmail.com>
2017-09-27 10:59:15 +0300

    back-end --> model phase Done

--- back-end/models/Job.js
+++ back-end/models/Job.js
@@ -1,3 +1,23 @@
 // TODO: Create and export a mongoose model called `Job` that follows the description in the README
+var mongoose = require('mongoose');
+var db = mongoose.createConnection('localhost','db')
 
+var Schema = mongoose.Schema;
+var JobSchema = new Schema({
+	company: String,
+	title: String,
+	description: String,
+	postedDate: Date,
+	salary: Number
 
+})
+
+var Job = db.model('Job',JobSchema);
+module.exports = Job;
+
+// object with the following properties:
+//   - `company` - The name of the company the job is for
+//   - `title` - The title of the job
+//   - `description` - A description of the job
+//   - `postedDate` - When the job became available
+//   - `salary` - The offered salary for the position
--- back-end/models/User.js
+++ back-end/models/User.js
@@ -1,7 +1,17 @@
 // TODO: Create and export a mongoose model called `User` whose schema would handle an object like `exampleUser`
+var mongoose = require('mongoose');
+var db = mongoose.createConnection('localhost','db')
+
+var Schema = mongoose.Schema;
+var UserSchema = new Schema({
+	name:  String,
+	email: String
+})
+
 var exampleUser = {
   name: 'Taka',
   email: 'taka@taka.com'
 };
 
-
+var User =db.model('User',UserSchema)
+module.exports = User;
