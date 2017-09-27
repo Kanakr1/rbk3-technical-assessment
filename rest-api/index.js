b0775b95965b5af48d1aeaa80bafa2f846bb9eed
maramalquraan <quraanmaram@gmail.com>
2017-09-27 15:18:47 +0300

    almost Done

--- rest-api/index.js
+++ rest-api/index.js
@@ -7,6 +7,74 @@ app.use(bodyParser.json());
 
 // YOUR CODE BELOW
 
+//Return JSON of all users 
+app.get('/api/users', function(req, res){
+ Users.getAll()
+	.then(function(error, users){
+		if(error){
+			throw error;
+		}
+		else{
+			res.json(users);
+
+		}
+	});
+});
+
+//Create new user* and return JSON of new user
+app.post('/api/users', function(req, res){
+  Users.addOne()
+  .then(function(error, user){
+		if(error){
+			throw error;
+		}
+		else{
+			res.json(user + " " + "added");
+
+		}
+	});
+});
+
+//Return JSON of single user with matching `id`
+app.get('/api/users/:id', function(req, res){
+	Users.getOne()
+	.then(id, function(error, user){
+		if(error){
+			throw error;
+		}
+		else{
+			res.json("User:" + " " +user)
+		}
+	});
+});
+
+
+
+// Update user** with matching `id` and return JSON of user
+app.put('/api/users/:id', function(req, res){
+	Users.updateOne()
+	.then(id, function(error, user){
+		if(error){
+			throw error;
+		}
+		else{
+			res.json(user + " " + "updated")
+		}
+	});
+});
+
+ // Delete user with matching `id` and return JSON of user
+app.delete('/api/users/:id', function(req, res){
+	Users.deleteOne()
+	.then(id, function(error, user){
+		if(error){
+			throw error;
+		}
+		else{
+			res.json(user + " " + "deleted")
+		}
+	});
+});
 
 
 // Do not touch this invocation of the `listen` method
