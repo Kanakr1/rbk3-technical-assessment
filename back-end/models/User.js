9977bc833f79a4baef15c722b28dffb0a5aeb5e2
maramshon <mralshon@gmail.com>
2017-09-27 10:22:00 +0300

    modefy User.js

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -4,4 +4,30 @@ var exampleUser = {
   email: 'taka@taka.com'
 };
 
+//don't forget to install mongoose .......................
 
+// Defining your schema
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  name:  String,
+  email: String
+  // there is no id in the example so I will not write it 
+});
+
+// Creating a model
+
+var User = mongoose.model('User', userSchema);
+
+//Connections
+
+mongoose.connect('localhost', 'gettingstarted');
+
+var connection = mongoose.createConnection('mongodb://localhost:27017/test'); // modefy this 
+
+var User = connection.model('User', userSchema);
+
+// export default User;
+export User;
