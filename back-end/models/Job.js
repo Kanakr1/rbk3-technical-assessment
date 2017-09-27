b073dac413bff43b73839ae3379b866a8f51c6c2
SirajKakeh <sirajonline.89@gmail.com>
2017-09-27 09:40:52 +0300

    models work

--- back-end/models/Job.js
+++ back-end/models/Job.js
@@ -1,3 +1,10 @@
 // TODO: Create and export a mongoose model called `Job` that follows the description in the README
+var mongoose = require('mongoose');
 
+var jobSchema = mongoose.Schema({
+    name: String,
+    email: String
+});
+var Job = mongoose.model('Job', jobSchema);
 
+module.exports.Job = Job;
--- back-end/models/User.js
+++ back-end/models/User.js
@@ -1,7 +1,23 @@
 // TODO: Create and export a mongoose model called `User` whose schema would handle an object like `exampleUser`
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('DB connected');
+});
+
+var userSchema = mongoose.Schema({
+    name: String,
+    email: String
+});
+var User = mongoose.model('User', userSchema);
+
 var exampleUser = {
   name: 'Taka',
   email: 'taka@taka.com'
 };
 
-
+module.exports.User = User;
