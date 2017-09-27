4a4e899ecf904d3310e167eb523bf68d919a315a
hibaAkroush <hiba.akr@gmail.com>
2017-09-27 16:43:04 +0300

    namedgraph solved

--- bare-node-graph/NamedGraph.js
+++ bare-node-graph/NamedGraph.js
@@ -1,9 +1,39 @@
 var Graph = require('./Graph.js');
 
-var NamedGraph = function (adjacencyListPath, name) {
+class NamedGraph extends Graph {
+	constructor(){
 	this.name = name
+		super()
+	}
+
 };
 
+NamedGraph.prototype = Object.create(Graph)
+NamedGraph.prototype.constructor = NamedGraph;
+NamedGraph.prototype.numberOfNodes = function () {
+return this.string.splite(" ").length;
+};
+
+
+NamedGraph.prototype.getEdges = function (node) {
+	lines = this.string.splite(" ");
+	for (var i = 0; i < lines.length; i++) {
+		if(lines[i][0]===node){
+			wantedLine = lines[i];
+		}
+	}
+	wantedArray = wantedLine.split("")
+	return wantedArray.splice(1);
+};
+NamedGraph.prototype.numberOfEdges = function () {
+	var counter = 0;
+	var nodes = getNodes()
+	for (var i = 0; i < nodes.length; i++) {
+		counter += this.getNumberEdges(nodes[i]);
+	}
+	numberOfNodes = this.numberOfNodes()
+	return counter - numberOfNodes;
+};
 
 
 module.exports = NamedGraph;
