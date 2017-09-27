e2e1a9451862e8a4a62bfd1275fa1129106963d1
osamaths <osama.alshbikat@gmail.com>
2017-09-27 16:56:46 +0300

    bare-node-graph finished

--- bare-node-graph/NamedGraph.js
+++ bare-node-graph/NamedGraph.js
@@ -1,9 +1,11 @@
 var Graph = require('./Graph.js');
 
 var NamedGraph = function (adjacencyListPath, name) {
+  Graph.call(this);
+  this.name = 'basic';
 };
 
-
+NamedGraph.prototype = Object.create(Graph.prototype);
+NamedGraph.prototype.constuctor = NamedGraph;
 
 module.exports = NamedGraph;
-
