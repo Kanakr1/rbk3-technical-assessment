7e5895d5a7243b5e00678232084fdd086e3a2b59
areejmayadeh <engareej94@gmail.com>
2017-09-27 09:42:38 -0400

    update pkg.json

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -1,7 +1,18 @@
 // TODO: Create and export a mongoose model called `User` whose schema would handle an object like `exampleUser`
 var exampleUser = {
-  name: 'Taka',
-  email: 'taka@taka.com'
+	name: 'Taka',
+	email: 'taka@taka.com'
 };
+
+var mongoose=require('mongoose');
+
+var UserSchema =mongoose.Schema({
+	name:String,
+	email: String
+});
+var User=mongoose.model('User',UserSchema);
+
+
+module.exports=User;
 
 
