3e8e8e5f3a184ef5d4186a5708b7fc71570c0115
ammarRBK <alshakmaskeain@gmail.com>
2017-09-27 13:59:19 +0300

    begin with front end (server)

--- front-end/public/server.js
+++ front-end/public/server.js
@@ -6,8 +6,15 @@ var User=require('../back-end/controllers/userController');
 var mongoose=require('mongoose');
 var Job=require('../back-end/controllers/jobController');
 var mongod=mongoose.connect;
+var db=require("../models/User");
+
+var port=process.env.Port||3000;
 app.use(express.static(path.join(__dirname+'public')));
 
+
+app.get('/',function(req,res){
+	res.sendFile(path.join(__dirname+'index.html'))
+})
 app.get('/api/users',function(req,res){
 	res.send(bodyparser.json(User.readAllUsers(function(data){
 		data=db.findAll(db.name)
@@ -15,7 +22,19 @@ app.get('/api/users',function(req,res){
 });
 
 app.get('/api/users/id',function(req,res){
-	res.send(bodyparser.json(User.getUserByName(req.bod.id,function(data){
-		var id=req.bod.id
+	res.send(bodyparser.json(User.getUserByName(req.body.id,function(data){
+		var Id=req.body.id;
+		if(Id===User.db.id){
+			return data;
+		}
+		res.send('Id is not in the database');
+
 	})))
 });
+
+// app.post()
+
+
+app.listen(port);
+
+console.log('listning on port: ',port)
