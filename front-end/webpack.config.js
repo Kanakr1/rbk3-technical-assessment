43086a1194f592517b807dcf1ffd5675be5486dd
SirajKakeh <sirajonline.89@gmail.com>
2017-09-27 16:14:48 +0300

    React Welcome Page Running

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -1,7 +1,21 @@
+const path = require('path');
 module.exports = {
-  entry: './public/src/index.js',
+  entry: './public/src/index.jsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public/compiled')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
   }
 };
