65c7b04e576a57c6903529b21f46ccd43b9203f4
rbkEsraaRamahi <israma7i@yahoo.com>
2017-09-27 14:14:58 +0300

    done rest

--- rest-api/index.js
+++ rest-api/index.js
@@ -7,6 +7,20 @@ app.use(bodyParser.json());
 
 // YOUR CODE BELOW
 
+app.get('/models/users', function(req, res){
+  res.json('users');
+
+app.post('/models/users', function(req, res){
+  res.json('addOne');
+
+app.get('/models/users/:id', function(req, res){
+  res.json('getOne');
+
+app.put('/models/users:id', function(req, res){
+  res.json('updateOne');
+
+app.delete('/models/users/:id', function(req, res){
+  res.json('deleteOne');
 
 
 // Do not touch this invocation of the `listen` method
