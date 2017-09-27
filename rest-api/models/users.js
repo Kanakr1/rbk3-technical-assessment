a415db36c3a1537b4b74ea5f027f2372ed275cc9
hibaAkroush <hiba.akr@gmail.com>
2017-09-27 14:40:27 +0300

    working on express

--- rest-api/models/users.js
+++ rest-api/models/users.js
@@ -42,7 +42,7 @@ exports.setAll = function (newUsers) {
 exports.getOne = function (id) {
   for (var i = 0; i < users.length; i++) {
     if (users[i].id===id) {
-      return user[i]
+      return users[i]
     }
   }
 };
@@ -51,15 +51,42 @@ exports.getOne = function (id) {
 exports.addOne = function (user) {
   users.push(user)
 };
-
+// i dont know how many inputs the user wants to update 
+// i will store the properties  in an array
+// also im not sure in which order will the user input data
+//i wll search each argument for @ symbol
+// if @ is found the argument is the email
 exports.updateOne = function (id, newProperties) {
+array = arguments
+var id = array[0];
+var x = array[1];
+var y = array[2];
+var email;
+var name;
+if(x.indexOf('@') > -1 && x.indexOf('.com')> -1){
+// x is not the email then y is
+email = y
+name = x
+}else{
+// x is the email then y is name
+email = x
+name = y  
+}
+// find the user and change the properties 
+for (var i = 0; i < users.length; i++) {
+  if (users[i].id===id) {
+    user[i].name = name;
+    user[i].email = email;
+    return user[i] 
+  }  
+}
 
 };
 
 exports.deleteOne = function (id) {
   for (var i = 0; i < users.length; i++) {
   if (users[i].id===id) {
-    
+    users.splice(i,1)
   }  
   }
 };
