ad2317290e06912079419d79f65b7a7f9d1ca72e
Haya-AlDineh <haya.aldineh@gmail.com>
2017-09-27 11:20:47 +0300

    first changes back-end

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -1,7 +1,17 @@
 
+
+var User = require('User')
+
+//this part is not that familiar with, i'll try my best with it and i will work on it later too.
+
 // Given the name of a user, retrieve their record from the database
 exports.getUserByName = function (name, callback) {
   // TODO
+  User.findOne({'name': 'name'}, callback(err, user));
+  // i need to serch by the input that is entered, but with this search it will consider 'name' as string, which is not what we need, so we need a way to insert the input in the search.
+
+  
+
 };
 
 // Given the name of a user, update their `email` property
