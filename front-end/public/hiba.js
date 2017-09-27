c76401d01844e3f51a0f2e9d3f7c5314b29d342d
hibaAkroush <hiba.akr@gmail.com>
2017-09-27 12:27:49 +0300

    working on frontend

--- front-end/public/hiba.js
+++ front-end/public/hiba.js
@@ -10,18 +10,23 @@ var myApp = angular.module('myApp',[]);
 // to the database and rendered along with previous data
 
 // in case the path is with a specific user 
-myApp.controller('myApp', function($scope,$http) {
+myApp.controller('appli', function($scope,$http) {
   // i assume response from server has data
-	$http.get("api/users",function(response){
-		$scope.users= response.data
+	$http.get("api/users").then(function(response){
+		$scope.users = response.data
 	})
 // i will go to html now and write code to render this
-	// now i will work on what happens when user clicks 
+	// now i will work on what happens when user clicks
+	//input is an object that contains username and email 
 	$scope.add= function(){
-		$http.post('api/users', input.user)
+		console.log(input.users)
+		$http.post('api/users', input.users)
 	}
 // maybe whatever i get from server is sorted there
 // i mean when i want one user i will get one user from server
 // when i want all users i will get all
 // here in front end i only need to worry about organizing and rendering 
+	$http.get("api/users/:id").then(function(response){
+		$scope.users= id
+	})
 });
