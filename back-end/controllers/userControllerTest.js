f0183dcdedf18339c7274ba6f5b0087efa815a8a
rbkEsraaRamahi <israma7i@yahoo.com>
2017-09-27 16:52:37 +0300

    last version back end

--- back-end/controllers/userControllerTest.js
+++ back-end/controllers/userControllerTest.js
@@ -54,6 +54,11 @@ describe('User Controller', function () {
   it('should have a method that given the name of a user, retrieves their record from the database', function (done) {
     // TODO: Write test(s) for a method exported by `userController` that behaves as described one line above
     // HINT: The `done` passed in is quite important...
+
+    it('should be a method', function () {
+      expect(exports.getUserByName).to.be.a('method');
+    });
+  
   });
 
   it('should have a method that given the name of a user, updates their `email` property', function (done) {
