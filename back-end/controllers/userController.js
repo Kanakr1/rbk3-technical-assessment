5e34ffbfa6524e3f9f790b91f1ebc566447fa65e
tariq-alhariri <tariqahmad1987@gmail.com>
2017-09-27 11:15:12 +0300

    after adding the controllers

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -2,14 +2,20 @@
 // Given the name of a user, retrieve their record from the database
 exports.getUserByName = function (name, callback) {
   // TODO
+  db.User.find( { name: {$exists : true} } )
 };
 
 // Given the name of a user, update their `email` property
 exports.updateEmailByName = function (name, newEmail, callback) {
   // TODO
+  db.User.findOneAndUpdate(
+   { "name" : name },
+   { $inc: { "email" : newEmail} }
+)
 };
 
 // Read all users from the database at once
 exports.readAllUsers = function (callback) {
   // TODO
+  db.User.find()
 };
