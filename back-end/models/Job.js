74e2369011a1dc3c21f72b7ae9f56bbe1baece3b
maimai84 <maialmasalmah@gmail.com>
2017-09-27 11:19:58 +0300

    first

--- back-end/models/Job.js
+++ back-end/models/Job.js
@@ -1,3 +1,9 @@
 // TODO: Create and export a mongoose model called `Job` that follows the description in the README
+var mongoose = require('mongoose');
+var schema=mongoose.Schema;
+var jobSchema=new schema({
+	job:String
+})
 
-
+var Job=mongoose.Model('Job',jobSchema)
+Module.export=Job;
