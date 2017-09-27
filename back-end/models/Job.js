1c43298e008da47bdd15b1d8e2198f1a9da96303
duhaali <dohaali415@gmail.com>
2017-09-27 11:19:20 +0300

    first edit

--- back-end/models/Job.js
+++ back-end/models/Job.js
@@ -1,3 +1,16 @@
 // TODO: Create and export a mongoose model called `Job` that follows the description in the README
 
+var mongoose=require('mongoose');
+var Schema=mongoose.Schema;
+mongoose.connect('mongodb://localhost/Myapp');
+mongoose.connect('mongodb://username:password@host:port/database?options...');
+
+var jobrschema=new Schema({
+	company:'string',
+	title:'string' ,
+	description: 'string',
+	postedDate:'boolean',
+	salary:'number'});
+
+var Job=mongoose.model(Job,jobrschema)
 
--- back-end/models/User.js
+++ back-end/models/User.js
@@ -1,7 +1,19 @@
 // TODO: Create and export a mongoose model called `User` whose schema would handle an object like `exampleUser`
+var mongoose=require('mongoose');
+var Schema=mongoose.Schema;
+mongoose.connect('mongodb://localhost/Myapp');
+mongoose.connect('mongodb://username:password@host:port/database?options...');
+
 var exampleUser = {
   name: 'Taka',
   email: 'taka@taka.com'
 };
 
+var userschema=new Schema({name: 'string',email: 'string'});
 
+var User= mongoose.model('User',userschema);
+var perantschema= new User(exampleUser);
+
+// var User=new User (exampleUser)
+
+module.exports=mongoose;
