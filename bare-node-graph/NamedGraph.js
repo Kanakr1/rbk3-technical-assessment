a888027cdd2e210d1c7e13ac2a0718fab6cab14e
Firas Firasesraa Esraa <firasesraa@RBK5s-Mac-mini.local>
2017-09-27 12:44:21 +0300

    Second Question 80%

--- bare-node-graph/NamedGraph.js
+++ bare-node-graph/NamedGraph.js
@@ -1,8 +1,12 @@
 var Graph = require('./Graph.js');
 
 var NamedGraph = function (adjacencyListPath, name) {
+	Graph.call(this, adjacencyListPath)
+	this.name = name
 };
 
+NamedGraph.prototype = Graph.prototype;
+NamedGraph.prototype.constructor = NamedGraph;
 
 
 module.exports = NamedGraph;
