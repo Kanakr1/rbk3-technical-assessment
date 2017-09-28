1a30137dda60389e2808717e0954fd80a4a78a2e
esraahusamia <esraa.hus93@yahoo.com>
2017-09-27 16:53:48 -1200

    back

--- back-end/controllers/jobController.js
+++ back-end/controllers/jobController.js
@@ -1,9 +1,11 @@
 var mongoose = require("mongoose");
 var Job = require('../models/Job');
+
 // Create a new job in the database
 exports.createJob = function (job, callback) {
-	var query ="insert into Job job"
-    User.findOrCreat (query, function (err,data){
+  var Job = new Job()
+  var query ="insert into Job "+job;
+  Job.findOrCreat (query, function (err,data){
   	if (data){
   		return data
   	}
@@ -15,8 +17,8 @@ exports.createJob = function (job, callback) {
 
 // Get all jobs that have a salary greater than $50,000
 exports.getHighPayingJobs = function (callback) {
-var query= "select * from Job where salary > 50000";
- User.findOn(query, function (err,data){
+  var query= "select * from Job where salary > 50000";
+  Job.findOn(query, function (err,data){
   	if (data){
   		return data
   	}
@@ -25,4 +27,3 @@ var query= "select * from Job where salary > 50000";
   	}
   })
 };
-}
--- back-end/controllers/jobControllerTest.js
+++ back-end/controllers/jobControllerTest.js
@@ -21,8 +21,34 @@ describe('Job Controller', function () {
 
   beforeEach(function (done) {
     clearDB(function () {
-      // TODO: Seed database with some jobs to run tests against. 
-    });
+          // TODO: Seed database with some jobs to run tests against. 
+          var jobs = [
+          { 
+            company : 'google' ,
+            title: 'prgrammer' ,
+            description: 'javaScript programmer' ,
+            postedDate: '1/12/2017' ,
+            salary: '1500$'
+
+          },
+          { 
+            company : 'microsft' ,
+            title: 'prgrammer' ,
+            description: 'desktop programmer' ,
+            postedDate: '5/12/2017' ,
+            salary: '2000$'
+
+          },
+          {
+            company : 'manseer' ,
+            title: 'manger' ,
+            description: 'manegar' ,
+            postedDate: '5/12/2017' ,
+            salary: '60000$'
+
+          }
+          ]
+        });
   });
 
   // TODO: Write your tests for jobController here
@@ -32,12 +58,26 @@ describe('Job Controller', function () {
   // HINT: The `done` passed in is quite important...
   // add a new recourd to the database 
   //the input should have the value to all column 
-  });
-   it('should have a method that return  all the records  the database that have the salary > 50000 $', function (done) {
+  createJob({company : 'RBK' ,
+            title: 'prgrammer' ,
+            description: 'FrontEnd programmer' ,
+            postedDate: '5/12/2017' ,
+            salary: '2000$'}).should.addTo(jobs)
+});
+  it('should have a method that return  all the records  the database that have the salary > 50000 $', function (done) {
   // TODO: Write test(s) for a method exported by `userController` that behaves as described one line above
   // HINT: The `done` passed in is quite important...
   // return all recourds with all data when the salary > 50000
-  })
 
-  });
+  getHighPayingJobs().should.equle(  {
+            company : 'manseer' ,
+            title: 'manger' ,
+            description: 'manegar' ,
+            postedDate: '5/12/2017' ,
+            salary: '60000$'
+
+          })
+
+
+});
 
